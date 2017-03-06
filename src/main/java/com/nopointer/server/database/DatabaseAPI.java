9b795751e0536ecd042d9ed384dccde323946de7
Kirill <Petrovich_1811@mail.ru>
2017-03-06 23:47:31 +0300

    Обновлён API

--- src/main/java/com/nopointer/server/database/DatabaseAPI.java
+++ src/main/java/com/nopointer/server/database/DatabaseAPI.java
@@ -2,33 +2,29 @@ package com.nopointer.server.database;
 
 import com.nopointer.server.entity.Commit;
 
+import java.sql.Connection;
 import java.util.List;
 
 public interface DatabaseAPI {
+    void setConnection(Connection connection);
     // Users
-    void registerUser(String login, String password);
-
+    boolean registerUser(String login, String password);
+    void deleteUser(String login);
     boolean login(String login, String password);
 
     // Texts
-    void createFile(String title, String text);
-
+    boolean createFile(String title);
     String getTitle(int idFile);
+    String getFileStatus(int idFile);
+    boolean changeFileStatus(int idFile);
 
-    List<Integer> getAllFilesId();
-
-    String getFileStatus();
-
-    void changeFileStatus();
+    // Access
+    List<Integer> getAllFilesId(String login);
 
     // Commit
-    String getActualText(int idFile);
-
+    List<String> getActualText(int idFile);
     Commit getCommitByDate(int idFile, String date);
-
     List<Integer> getAllCommitsId(int idFile);
-
-    void addCommit(int idFile, String login, String text);
-
+    boolean addCommit(int idFile, String login, List<String> text);
 
 }
