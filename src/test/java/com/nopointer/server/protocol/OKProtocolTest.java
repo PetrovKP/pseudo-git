ba9304c4c0b4fea5be859a07a40b84b1ff1992f7
vgvoleg <vgvoleg@mail.ru>
2017-03-07 13:33:36 +0300

    Added probably all tests for protocol

--- src/test/java/com/nopointer/server/protocol/OKProtocolTest.java
+++ src/test/java/com/nopointer/server/protocol/OKProtocolTest.java
@@ -3,6 +3,7 @@ package com.nopointer.server.protocol;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.nopointer.server.config.TestModule;
+import com.nopointer.server.entity.Commit;
 import com.nopointer.server.protocol.entity.Request;
 import com.nopointer.server.protocol.entity.Response;
 import org.junit.Before;
@@ -11,6 +12,7 @@ import org.junit.Test;
 import java.util.*;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class OKProtocolTest
 {
@@ -141,5 +143,110 @@ public class OKProtocolTest
         assertEquals(status, "locked");
     }
 
+    @Test
+    public void successChangeFileStatus() throws Exception {
+        int idFile = 1;
+
+        Request request = new Request("changeFileStatus", idFile);
+        Response response = protocol.handleRequest(request);
+
+        assertEquals(response.getCode(), 100);
+    }
+
+    @Test
+    public void unsuccessChangeFileStatus() throws Exception {
+        int idFile = 1;
+
+        Request request = new Request("changeFileStatus", idFile);
+        Response response = protocol.handleRequest(request);
+
+        assertEquals(response.getCode(), 200);
+    }
+
+    @Test
+    public void getAllFilesId() throws Exception {
+        List<String> auth = new ArrayList<>();
+        auth.add("Oleg");
+
+        Request request = new Request("getAllFilesId", auth);
+        Response response = protocol.handleRequest(request);
+
+        List<Integer> list = (List<Integer>)response.getData().get(0);
+
+        assertEquals((int)list.get(0), 1);
+        assertEquals((int)list.get(1), 2);
+    }
+
+    @Test
+    public void getActualText() throws Exception {
+        int idFile = 1;
+
+        Request request = new Request("getFileStatus", idFile);
+        Response response = protocol.handleRequest(request);
+
+        List<String> text = (List<String>)response.getData().get(0);
+
+        assertEquals(text.get(0), "Text");
+        assertEquals(text.get(1), "Here");
+    }
+
+    @Test
+    public void getCommitByDate() throws Exception {
+        int idFile = 1;
+
+        List<String> list1 = new ArrayList<>();
+        list1.add("Text");
+        list1.add("Here");
+        List<String> list2 = new ArrayList<>();
+        list2.add("Text1");
+        list2.add("Here1");
+        Commit commit1 = new Commit(list1, list2);
+
+        Request request = new Request("getCommitByDate", idFile);
+        Response response = protocol.handleRequest(request);
+
+        Commit commit2 = (Commit)response.getData().get(0);
+
+        assertTrue(commit1.equals(commit2));
+    }
+
+    @Test
+    public void getAllCommitsId() throws Exception {
+        int idFile = 3;
+
+        Request request = new Request("getAllCommitsId", idFile);
+        Response response = protocol.handleRequest(request);
+
+        List<Integer> list = (List<Integer>)response.getData().get(0);
+
+        assertEquals((int)list.get(0), 1);
+        assertEquals((int)list.get(1), 2);
+    }
+
+    @Test
+    public void successAddCommit() throws Exception {
+        int idFile = 1;
+        List<String> auth = new ArrayList<>();
+        auth.add("Oleg");
+        auth.add("Ovcharuk");
+
+        Request request = new Request("addCommit", idFile, auth, null);
+        Response response = protocol.handleRequest(request);
+
+        assertEquals(response.getCode(), 100);
+    }
+
+    @Test
+    public void unsuccessAddCommit() throws Exception {
+        int idFile = 2;
+        List<String> auth = new ArrayList<>();
+        auth.add("Oleg");
+        auth.add("Ovcharuk");
+
+        Request request = new Request("addCommit", idFile, auth, null);
+        Response response = protocol.handleRequest(request);
+
+        assertEquals(response.getCode(), 200);
+    }
 
 }
