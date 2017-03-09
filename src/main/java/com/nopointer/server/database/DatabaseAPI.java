d1bbfc4488c8ef49e4c54167dcb5055f04636e43
Kirill <Petrovich_1811@mail.ru>
2017-03-09 16:47:41 +0300

    обновлён интерфейс

--- src/main/java/com/nopointer/server/database/DatabaseAPI.java
+++ src/main/java/com/nopointer/server/database/DatabaseAPI.java
@@ -7,24 +7,32 @@ import java.util.List;
 
 public interface DatabaseAPI {
     void setConnection(Connection connection);
+
     // Users
     boolean registerUser(String login, String password);
-    void deleteUser(String login);
     boolean login(String login, String password);
 
-    // Texts
-    boolean createFile(String title);
-    String getTitle(int idFile);
-    String getFileStatus(int idFile);
-    boolean changeFileStatus(int idFile);
+    @Deprecated
+    boolean deleteUser(String login);
 
-    // Access
+    // Files
+    boolean createFile(String login, String title, List<String> text); // Я на уровне клиента запрещу создать пустой по содержанию файл
+    Integer getCommitsCount(String login, int idFile);
+    String getTitle(String login, int idFile);
     List<Integer> getAllFilesId(String login);
+    List<String> getActualText(String login, int idFile);
+    List<Integer> getAllCommitsId(String login, int idFile);
 
-    // Commit
-    List<String> getActualText(int idFile);
-    Commit getCommitByDate(int idFile, String date);
-    List<Integer> getAllCommitsId(int idFile);
-    boolean addCommit(int idFile, String login, List<String> text);
+    //Access
+    List<String> getAllUsersByFile(String login, int idFile);
+    String getFileStatus(String login, int idFile);
+    boolean addUserToFile(String login, String newUserLogin, int idFile);
+    boolean changeFileStatus(String login, int idFile);
+
+    // Commits
+    String getCommitDateById(String login, int idCommit);
+    Commit getCommitById(String login, int idFile, int idCommit);
+    boolean addCommit (String login, int idFile, List<String> text);
+    boolean revertFileToCommit (String login, int idFile, int idCommit);
 
 }
