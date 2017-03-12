866b8abc8870b475b953f8454a6e9e2a24cdb81b
Kirill <PetrovKP@users.noreply.github.com>
2017-03-12 20:14:25 +0300

    Revert "hotfix of module usages"

--- src/test/java/com/nopointer/server/database/DatabaseImplTest.java
+++ src/test/java/com/nopointer/server/database/DatabaseImplTest.java
@@ -2,18 +2,19 @@ package com.nopointer.server.database;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.nopointer.server.config.Module;
+import com.nopointer.server.config.TestModule;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class DatabaseImplTest {
     private Database database;
 
     @Before
     public void setUp() throws Exception {
-        Injector injector = Guice.createInjector(new TestDatabaseModule());
+        Injector injector = Guice.createInjector(new TestModule());
         database = injector.getInstance(Database.class);
     }
 
