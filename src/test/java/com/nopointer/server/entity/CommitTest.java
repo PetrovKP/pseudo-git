1db293766da93f21df7f83e7a9cf1e32654e05c4
vgvoleg <vgvoleg@mail.ru>
2017-03-12 20:19:09 +0300

    hotfix of module usages

--- src/test/java/com/nopointer/server/entity/CommitTest.java
+++ src/test/java/com/nopointer/server/entity/CommitTest.java
@@ -122,12 +122,4 @@ public class CommitTest {
         assertFalse(commit1.equals(commit2));
         assertFalse(commit2.equals(commit1));
     }
-
-    @Test
-    public void nd() throws Exception {
-        Commit commit = new Commit(new ArrayList<String>(), newText);
-        for (TextString ts : commit.getTextStrings()) {
-            System.out.println(ts.getString());
-        }
-    }
 }
